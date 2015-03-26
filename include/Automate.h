7f81a8706d5f70c7206b54ec40587ddac9b91980
Patrick Sebastian John von Freyend <sebastian-jvf@live.de>
2015-03-26 08:45:01 +0100

    Jérémy dit saluut

--- include/Automate.h
+++ include/Automate.h
@@ -6,6 +6,9 @@
 #include "Symbole.h"
 #include "Etat.h"
 #include "Lexer.h"
+#include <stack>
+#include <map>
+
 using namespace std;
 
 
@@ -14,11 +17,17 @@ class Automate
     public:
         Automate();
         void lecture(char* filename);
-        void decalage(Symbole* sym, Etat* etat);
+        void analyser();
+        void decalage(Etat* etat);
+        void reduire();
         virtual ~Automate();
     protected:
     private:
         Lexer lexer;
+        map<Etat*, map<TYPE, Etat*> > m_transitions;
+        Symbole* currentSym;
+        stack<Etat*> m_etats;
+        stack<Symbole*> m_symboles;
 };
 
 #endif // AUTOMATE_H

870333ff7a1e31c7b77e487789745bd9edd6ae7a
Mohamed Riadh Chérif <mohamed-riadh.cherif@insa-lyon.fr>
2015-04-03 09:36:45 +0200

    Update Declaration.h

--- include/Declaration.h
+++ include/Declaration.h
@@ -3,7 +3,23 @@
 #define __lg_diag_classe_Declaration_h
 
 #include <string>
-
+class Declaration
+{
+	public:
+		Declaration();
+		Declaration(std::string);
+		std::string getNom();
+		double getVal();
+		void setVal(double val);
+		void setNom(std::string nom);
+		
+	protected:
+	    bool m_isAffectee;
+		std::string id;
+		double val ;
+		bool m_isConst;
+	private:
+};
 class Declaration
 {
 	public:
@@ -14,10 +30,15 @@ class Declaration
 		void setNom(std::string nom);
 		double getVal();
 		void setVal(double val);
+		
+		bool isConst(retuen m_isConst);
+		bool isAffectee(){return m_isAffectee;}
 	
 	protected:
 		std::string nom;
 		double val ;
+		bool m_isAffectee;
+		bool m_isConst;
 	private:
 };
 
